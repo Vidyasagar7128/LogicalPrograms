a57ceb682848e5b8ea86fc701a9acbb1423ab47a
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-06 08:23:28 +0530

    Added : Newtons sqrt method

--- LogicalPrograms/CustomPrograms.cs
+++ LogicalPrograms/CustomPrograms.cs
@@ -8,47 +8,22 @@ namespace LogicalPrograms
 {
     class CustomPrograms
     {
-      		public static void dayOfWeeks(String day, String month, String year)
-		{
-			int d = int.Parse(day);
-			int m = int.Parse(month);
-			int y = int.Parse(year);
-			int y0 = y - (14 - m) / 12;
-			Console.WriteLine($"y0 = y - (14 - m) / 12 : {y - (14 - m) / 12}");
-			int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
-			Console.WriteLine($"y0 + y0 / 4 - y0 / 100 + y0 / 400 :{x}");
-			int m0 = m + 12 * ((14 - m) / 12) - 2;
-			Console.WriteLine($"m + 12 * ((14 - m) / 12) - 2 :{m0}");
-			int d0 = (d + x + 31 * m0 / 12) % 7;
-			Console.WriteLine($"(d + x + 31 * m0 / 12) % 7 : {d0}");
-			switch (d0)
-			{
-				case 0:
-					Console.WriteLine("sunday");
-					break;
-				case 1:
-					Console.WriteLine("monday");
-					break;
-				case 2:
-					Console.WriteLine("tuesday");
-					break;
-				case 3:
-					Console.WriteLine("wednsday");
-					break;
-				case 4:
-					Console.WriteLine("thursday");
-					break;
-				case 5:
-					Console.WriteLine("friday");
-					break;
-				case 6:
-					Console.WriteLine("saturday");
-					break;
+       public double squareRoot(double n, double l)
+        {
+            double x = n;
+            double root;
+            int count = 0;
+            while (true)
+            {
+                count++;
 
-				default:
-					Console.WriteLine("invalid output");
-					break;
-			}
-		}
-	}
+                root = 0.5 * (x + (n / x));
+
+                if (Math.Abs(root - x) < l)
+                    break;
+                x = root;
+            }
+            return root;
+        }
+    }
 }
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,7 +7,12 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Enter Number : ");
-            CustomPrograms.dayOfWeeks(args[0],args[1],args[2]);
+            double num1 = int.Parse(Console.ReadLine());
+            double num2 = int.Parse(Console.ReadLine());
+            double n = 327;
+            double l = 0.00001;
+            CustomPrograms customPrograms = new CustomPrograms();
+            Console.WriteLine(customPrograms.squareRoot(num1,num2));
         }
     }
 }
